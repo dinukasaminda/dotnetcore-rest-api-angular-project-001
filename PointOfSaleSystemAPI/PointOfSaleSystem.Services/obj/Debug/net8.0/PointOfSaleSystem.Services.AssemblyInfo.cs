2e53ae5d2c7479c6ab3fb533df495c5ed54b6c09
Dinuka Bandara <dinuka@vexprotech.com>
2023-12-15 19:33:30 +0530

    add vs git ignores

--- PointOfSaleSystemAPI/PointOfSaleSystem.Services/obj/Debug/net8.0/PointOfSaleSystem.Services.AssemblyInfo.cs
+++ PointOfSaleSystemAPI/PointOfSaleSystem.Services/obj/Debug/net8.0/PointOfSaleSystem.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PointOfSaleSystem.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12dec48d1771407109ef5285cf5e8ba1d125977b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PointOfSaleSystem.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PointOfSaleSystem.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
