8a011b27498fa53be6b9eb912725ab55c54596c2
Dinuka Bandara <dinuka@vexprotech.com>
2023-12-15 20:00:01 +0530

    docker composer

--- PointOfSaleSystemAPI/PointOfSaleSystem.Models/obj/Debug/net8.0/PointOfSaleSystem.Models.AssemblyInfo.cs
+++ PointOfSaleSystemAPI/PointOfSaleSystem.Models/obj/Debug/net8.0/PointOfSaleSystem.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PointOfSaleSystem.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12dec48d1771407109ef5285cf5e8ba1d125977b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PointOfSaleSystem.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PointOfSaleSystem.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
